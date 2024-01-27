32d52ad01660eb7c8cb0827cf87c94c23770e1dd
hienvl125 <hienviluong125@gmail.com>
2024-01-27 18:46:00 +0700

    chore(fe): insert new final new line for some core files

--- fe/src/pages/login.tsx
+++ fe/src/pages/login.tsx
@@ -1,7 +1,7 @@
 export default function Login() {
-    return (
-      <main>
-       This is Login page
-      </main>
-    )
-  }
+  return (
+    <main>
+      This is Login page
+    </main>
+  )
+}
